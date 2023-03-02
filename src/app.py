13a14ac24d5730ce89357f3dce6cd370bb0c35c3
Jesper Bergström <jes.bergstrom@gmail.com>
2023-03-02 19:20:30 +0200

    lutakko.py: added return value. Web app example modified to work with lutakko scraper

--- src/app.py
+++ src/app.py
@@ -1,29 +1,16 @@
 from flask import Flask, jsonify, request
 
+from scrapers.lutakko import scrape_lutakko
+
+
 app = Flask(__name__)
 
+tapahtumat = scrape_lutakko()
 
-countries = [
-    {"id": 1, "name": "Thailand", "capital": "Bangkok", "area": 513120},
-    {"id": 2, "name": "Australia", "capital": "Canberra", "area": 7617930},
-    {"id": 3, "name": "Egypt", "capital": "Cairo", "area": 1010408},
-]
+@app.get("/events")
+def get_events():
+    return tapahtumat
 
-def _find_next_id():
-    return max(country["id"] for country in countries) + 1
-
-@app.get("/countries")
-def get_countries():
-    return jsonify(countries)
-
-@app.post("/countries")
-def add_country():
-    if request.is_json:
-        country = request.get_json()
-        country["id"] = _find_next_id()
-        countries.append(country)
-        return country, 201
-    return {"error": "Request must be JSON"}, 415
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/scrapers/lutakko.py
+++ src/scrapers/lutakko.py
@@ -1,71 +1,75 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-from event import Event
+from scrapers.event import Event
 import json
 
-events = []
 
-# lutakon keikkojen sivu
-resp = requests.get('https://jelmu.net').text
+def scrape_lutakko():
+    events = []
 
-# testauksessa käytetty lokaalia html-dokumenttia
-# että liian monet http-pyynnöt ei johda ip-blokkaukseen
-# HTMLFile = open("lutakko.html", "rb")
-# html = HTMLFile.read()
+    # lutakon keikkojen sivu
+    resp = requests.get('https://jelmu.net').text
 
-soup = BeautifulSoup(resp, features="html.parser")
+    # testauksessa käytetty lokaalia html-dokumenttia
+    # että liian monet http-pyynnöt ei johda ip-blokkaukseen
+    # HTMLFile = open("lutakko.html", "rb")
+    # html = HTMLFile.read()
 
-# eventit listattuna li-tägeihin
-li = soup.find_all('li')
+    soup = BeautifulSoup(resp, features="html.parser")
 
-# käy läpi kaikki tapahtumat
-for i in li:
-    if i.a.span is None:
-        continue
+    # eventit listattuna li-tägeihin
+    li = soup.find_all('li')
 
-    # alusta event
-    event = Event("", 0, 0, 0, 0, 0, None)
-    # päivämäärä
-    date = i.find('div', {'class':'date'}).span.text
-    # ikäraja
-    age = i.find('div', {'class':'age-limit'})
-    # kellonaika
-    time = i.find('div', {'class':'time'}).span.text
-    # lippujen hinta
-    tickets = i.find('div', {'role':'tickets'})
-    idx = tickets.text.find('â‚¬')
-    prices = re.sub(r"[\n\t]*", "", tickets.text[0:idx])
-    priceRange = re.findall(r'\d+', prices)
+    # käy läpi kaikki tapahtumat
+    for i in li:
+        if i.a.span is None:
+            continue
 
-    # handlaa jos ikäraja-elementtiä ei ole
-    if age is None:
-       age = "Ei ikärajaa"
-    else:
-       age = age.span.text
+        # alusta event
+        event = Event("", 0, 0, 0, 0, 0, None)
+        # päivämäärä
+        date = i.find('div', {'class':'date'}).span.text
+        # ikäraja
+        age = i.find('div', {'class':'age-limit'})
+        # kellonaika
+        time = i.find('div', {'class':'time'}).span.text
+        # lippujen hinta
+        tickets = i.find('div', {'role':'tickets'})
+        idx = tickets.text.find('â‚¬')
+        prices = re.sub(r"[\n\t]*", "", tickets.text[0:idx])
+        priceRange = re.findall(r'\d+', prices)
 
-    # aseta eventille attribuuttien arvot
-    event.price = '-'.join([min(priceRange, key=int), max(priceRange, key=int)])
-    event.agelimit = age
-    event.day = date.split(" ")[1].split(".")[0]
-    event.month = date.split(event.day)[1].replace('.', '')
-    event.tstart = time.split('-')[0]
-    event.tend = time.split('-')[1]
+        # handlaa jos ikäraja-elementtiä ei ole
+        if age is None:
+            age = "Ei ikärajaa"
+        else:
+            age = age.span.text
 
-    # esiintyjätiedot
-    spans = i.a.find_all('span')
-    names = []
+        # aseta eventille attribuuttien arvot
+        event.price = '-'.join([min(priceRange, key=int), max(priceRange, key=int)])
+        event.agelimit = age
+        event.day = date.split(" ")[1].split(".")[0]
+        event.month = date.split(event.day)[1].replace('.', '')
+        event.tstart = time.split('-')[0]
+        event.tend = time.split('-')[1]
 
-    for span in spans:
-        # esiintyjät
-        names.append(span.text)
-    
-    event.name = ' '.join(names)
-    events.append(event)
+        # esiintyjätiedot
+        spans = i.a.find_all('span')
+        names = []
 
-lutakko = []
-for i in events:
-   lutakko.append(json.dumps(i.__dict__, ensure_ascii=False))
+        for span in spans:
+            # esiintyjät
+            names.append(span.text)
+        
+        event.name = ' '.join(names)
+        events.append(event)
+
+    lutakko = []
+    for i in events:
+        lutakko.append(json.dumps(i.__dict__, ensure_ascii=False))
+
+    return lutakko;
 
 # printtaa ekan eventin
 # print(lutakko[0])
