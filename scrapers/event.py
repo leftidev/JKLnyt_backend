2bfe8da0d1a9b2faba704f705e991a0618573a70
Jesper Bergström <jes.bergstrom@gmail.com>
2023-03-02 18:17:17 +0200

    Removed redundant scrapers/ -folder

--- scrapers/event.py
+++
@@ -1,9 +0,0 @@
-class Event:
-  def __init__(self, name, day, month, tstart, tend, price, agelimit):
-    self.name = name
-    self.day = day
-    self.month = month
-    self.tstart = tstart
-    self.tend = tend
-    self.price = price
-    self.agelimit = agelimit
