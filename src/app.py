9044e04579794855c9ca1c49e6cff727d80b35df
Jesper Bergström <jes.bergstrom@gmail.com>
2023-03-09 15:59:48 +0200

    Docker container: Created requirements.txt and Dockerfile

--- src/app.py
+++ src/app.py
@@ -5,7 +5,12 @@ from scrapers.lutakko import scrape_lutakko
 
 app = Flask(__name__)
 
+# Tässä skreipataan
 tapahtumat = scrape_lutakko()
+
+@app.route('/')
+def hello_world():
+    return '<h1>Hello from the JKLnyt team!</h2>'
 
 @app.get("/events")
 def get_events():
