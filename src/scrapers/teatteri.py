d206ae43b1ed9557f3248d18e5aae3f84f6a8d95
peltolma <peltolma@student.jyu.fi>
2023-03-17 13:54:31 +0200

    teatteri filu

---
+++ src/scrapers/teatteri.py
@@ -0,0 +1,17 @@
+from selenium import webdriver 
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+
+import time 
+ 
+driver = webdriver.Firefox()
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element(By.NAME, "q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+driver.close()
