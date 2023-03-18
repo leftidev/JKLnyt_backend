ad49eecb2a8f81cf32eb0c4afd1967c264af9c76
peltolma <peltolma@student.jyu.fi>
2023-03-18 12:37:07 +0200

    escape skreipperi valmis

---
+++ src/scrapers/escape.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+from event import Event
+import json
+
+
+def scrape_escape():
+    events = []
+
+    # escapen tapahtumasivu
+    resp = requests.get('https://escapejkl.fi/tapahtumat/').text
+
+    soup = BeautifulSoup(resp, features="html.parser")
+
+    # eventit listattuna p-tägeihin
+    ps = soup.find_all('div', {'class':'et_pb_text_inner'})[2]
+
+    # käy läpi kaikki tapahtumat
+    for p in ps:
+        eText = p.text
+        # suodata tyhjät pois
+        if (eText.strip() != ""):
+            event = Event("", 0, 0, 0, 0, 0, None, "")
+            textSplit = eText.split(" ", 1)
+            event.name = textSplit[1]
+            date = textSplit[0]
+            event.day = date.split(".")[0]
+            event.month = date.split(".")[1]
+            event.info = "https://escapejkl.fi/tapahtumat/"
+            event.agelimit = "Ei tiedossa"
+            event.price = "Ei tiedossa"
+            events.append(event)
+        
+
+    escape = []
+    for i in events:
+        escape.append(json.dumps(i.__dict__, ensure_ascii=False))
+
+    return escape
