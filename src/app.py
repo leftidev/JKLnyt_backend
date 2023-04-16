8228ce6ecfa56e19cf42b6c732a63e3caded509c
Jesper Bergström <jes.bergstrom@gmail.com>
2023-04-16 09:38:09 +0300

    Commented debug print out, better code comments

--- src/app.py
+++ src/app.py
@@ -39,7 +39,7 @@ def scrape_and_insert():
     collection.insert_many(tapahtumat_jjk)
     collection.insert_many(tapahtumat_escape)
     # Debug print
-    print('Data inserted to collection "events" succesfully')
+    #print('Data inserted to collection "events" succesfully')
 
 # Create a scheduler to run the scraper function every 24 hours
 scheduler = BackgroundScheduler()
@@ -49,8 +49,7 @@ scheduler.start()
 # Retrieves all the data from MongoDB collection, converts the _id field to a string (because it's not JSON serializable by default), and returns the data as a JSON response
 @app.route('/get', methods=['GET'])
 def get_data():
-    # Note that this is a simple way to add API key authentication, but it's not the most secure method. 
-    # A more secure approach would involve using a token-based authentication system, such as OAuth2.
+    # This is a simple API key authentication! A more secure approach would involve using a token-based authentication system, such as OAuth2.
     api_key = request.headers.get('X-API-KEY')
     if api_key != API_KEY:
         return jsonify({'error': 'Unauthorized access'}), 401
