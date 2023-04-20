57765e90385b3d869286c78e406e5c8038e30d50
peltolma <peltolma@student.jyu.fi>
2023-04-20 15:37:02 +0300

    lohi pvm check

--- src/scrapers/lohi.py
+++ src/scrapers/lohi.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import re
 from scrapers.event import Event
+# from event import Event
+from datetime import date
 import json
 
 
@@ -21,13 +23,28 @@ def scrape_lohi():
     for tr in trs:
         row = tr.find_all('td')
         home = row[1].text
+
+        # etsi vain kotiottelut
         if (home.lower() == 'lohi'):
+            # hae eventin ja nykyhetken dd/mm
             time = row[0]
             time = time.text.split(" ")[1].split(".")
+            dayVar = time[0]
+            monthVar = time[1]
+            today = date.today()
+
+            # jos eka event on jo mennyt, mene seuraavaan
+            if (len(events) == 0):
+                if (today.month > int(monthVar)):
+                    continue
+                if (today.month == int(monthVar)):
+                    if (today.day > int(dayVar)):
+                        continue
+
             event = Event("", 0, 0, 0, 0, 0, None, "", "", "", 0, 0)
             event.name = home + " vs " + row[2].text
-            event.day = time[0]
-            event.month = time[1]
+            event.day = dayVar
+            event.month = monthVar
             event.agelimit = "Ei ikärajaa"
             event.price = "Ei tiedossa"
             event.info = "https://jyvaskylanlohi.fi/ottelut/"
