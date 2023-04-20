0dd4c5a80226ae85857c3aa8e9db76cad56f0e8b
peltolma <peltolma@student.jyu.fi>
2023-04-20 15:48:48 +0300

    paviljonki pvm check

--- src/scrapers/paviljonki.py
+++ src/scrapers/paviljonki.py
@@ -2,6 +2,8 @@ import requests
 import re
 from bs4 import BeautifulSoup
 from scrapers.event import Event
+#from event import Event
+from datetime import date
 import json
 
 def scrape_paviljonki():
@@ -9,24 +11,33 @@ def scrape_paviljonki():
     events = []
 
     resp = requests.get('https://www.paviljonki.fi/tapahtumat-liput/').text
-    #print(resp)
-
-    # HTMLFile = open("paviljonki.html", "rb")
-    # html = HTMLFile.read()
 
     soup = BeautifulSoup(resp, features="html.parser")
     divs = soup.find_all("div", {"class": "event-list-item"})
 
     for d in divs:
         ahref = d.find("h2", {"class":"event-details__title"}).a
-        event = Event("", 0, 0, 0, 0, 0, None, "", "", "", 0, 0)
 
-        event.name = ahref.text.strip()
+        # hae tapahtuman ja nykyhetken pvm
         time = d.find("div", {"class": "event-details__meta"}).text.strip()
         times = time.splitlines()
-        date = times[-1].replace('\t', '')
-        event.day = date.split('.')[0]
-        event.month = date.split('.')[1]
+        dateVar = times[-1].replace('\t', '')
+        dayVar = dateVar.split('.')[0]
+        monthVar = dateVar.split('.')[1]
+        today = date.today()
+
+        # jos eka event on jo mennyt, mene seuraavaan
+        if (len(events) == 0):
+            if (today.month > int(monthVar)):
+                continue
+            if (today.month == int(monthVar)):
+                if (today.day > int(dayVar)):
+                    continue
+
+        event = Event("", 0, 0, 0, 0, 0, None, "", "", "", 0, 0)
+        event.name = ahref.text.strip()
+        event.day = dayVar
+        event.month = monthVar
         event.info = ahref.get("href")
         event.venue = "Paviljonki"
         event.lat = 62.24031954660093
